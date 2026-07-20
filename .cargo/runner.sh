ad192d26f96257245a43e52469f0f9403d2f0262
Fredrik Reinholdsen <fredrik.reinholdsen@gmail.com>
2026-07-20 22:23:09 +0200

    feat: Added check to runner script for compiled binary size.
    Cargo runner script will now check that the compiled master binary is small enough to fit into flash memory prior to flashing procedure

--- .cargo/runner.sh
+++ .cargo/runner.sh
@@ -1,5 +1,23 @@
 #!/bin/sh
 
+check_bin_size() {
+  # Checks that the compiled binary is small enough to fit into flash memory (256kB)
+  # If not, an error is raised and script will exit.
+  MAX=$((256 * 1024))
+
+  BIN=$(mktemp)
+  trap "rm -f $BIN" EXIT
+  rust-objcopy -O binary $1 "$BIN"
+  SIZE=$(stat -c%s "$BIN")
+
+  if [ "$SIZE" -gt "$MAX" ]; then
+    echo "ERROR: binary is $((SIZE / 1024))kB, exceeds 256kB limit" >&2
+    exit 1
+  fi
+
+  return 0
+}
+
 has_rp2350_bootloader() {
   # Returns 0 (true) if 2e8a:000f is present, 1 (false) otherwise
   if lsusb | grep -qi '2e8a:000f'; then
@@ -9,11 +27,14 @@ has_rp2350_bootloader() {
   fi
 }
 
+# ---------- main ----------
+check_bin_size "$1"
+
 # Example usage:
 if has_rp2350_bootloader; then
   echo "RP2350 USBbootloader is connected! Flashing over USB..."
-  elf2uf2-rs deploy -t --family rp2350-arm-s $1
+  elf2uf2-rs deploy -t --family rp2350-arm-s "$1"
 else
   echo "No RP2350 bootloader not detected, running DAP"
-  probe-rs run --chip RP235x $1
+  probe-rs run --chip RP235x "$1"
 fi
